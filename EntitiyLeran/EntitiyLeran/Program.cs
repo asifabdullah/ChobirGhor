abf3d2a0a3e518815d82c977d23744b9ec2922f0
asifabdullah <asif.ndc@gmail.com>
2016-04-26 18:00:20 +0600

    Commit

---
+++ EntitiyLeran/EntitiyLeran/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiyLeran
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HomeCinemaEntities db=new HomeCinemaEntities();
+            var dbSet = db.Movies;
+            List<Movie> movies = dbSet.ToList();
+            foreach (Movie movie in movies)
+            {
+                Console.WriteLine(movie.Director);
+            }
+            Console.Read();
+        }
+    }
+}
